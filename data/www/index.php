16f1711130d69eaafc179eee31cf2466d02e61e0
ngabesz-wse <gabor.nagy@innonic.com>
2017-09-28 09:31:56 +0200

    desc

--- data/www/index.php
+++ data/www/index.php
@@ -16,4 +16,19 @@ $customer->addRental($rental2);
 
 echo $customer->statement();
 
+echo '<br/> ---------------------------------- <br/>';
+
+$geza = new Customer('Géza');
+
+$movie3 = new Movie('Baywatch',1);
+$movie4 = new Movie('Thomas a gőzmozdony',2);
+
+$rental3 = new Rental($movie3,2);
+$rental4 = new Rental($movie4,6);
+
+$geza->addRental($rental3);
+$geza->addRental($rental4);
+
+echo $geza->statement();
+
 
